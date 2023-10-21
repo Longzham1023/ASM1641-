4f0729b58262556aa7c128bc0e2ded4ff58eecb5
PhongHoang68 <phonghnggch210556@fpt.edu.vn>
2023-10-22 02:44:21 +0700

    controller set up

---
+++ ASM1641-/Controllers/CustomerController.cs
@@ -0,0 +1,152 @@
+﻿using ASM1641_.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASM1641_.Controllers
+{
+    [Authorize]
+    [Route("api/v1/customer")]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _customerSerive;
+
+        public CustomerController(ICustomerService _customerSerive)
+        {
+            this._customerSerive = _customerSerive;
+        }
+
+
+        // GET: api/values
+        [HttpGet("viewcart"), Authorize(Roles = "User")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last()!;
+                string customerId = _customerSerive.GetIdByToken(token);
+                var cart = await _customerSerive.ViewCartUser(customerId);
+
+                return Ok(cart);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        //Add to cart
+        [HttpPost("addtocart"), Authorize(Roles = "User")]
+        public async Task<IActionResult> Post(string bookId, int quantity)
+        {
+            try
+            {
+                string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last()!;
+                string customerId = _customerSerive.GetIdByToken(token);
+                if (string.IsNullOrEmpty(bookId) || quantity <= 0)
+                {
+                    return BadRequest("Error: Invalid input fields");
+                }
+
+                await _customerSerive.AddToCart(customerId, bookId, quantity);
+                return Ok("Added item to cart successfully!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+        }
+
+        // PUT api/values/5
+        // update cart items
+        [HttpPut("updatecart/{id}"), Authorize(Roles = "User")]
+        public async Task<IActionResult> Put(string id, int quantity)
+        {
+            try
+            {
+                string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last()!;
+                if (string.IsNullOrEmpty(token))
+                {
+                    return Unauthorized("Token not found!");
+                }
+
+                if (string.IsNullOrEmpty(id) || quantity <= 0)
+                {
+                    return BadRequest($"Error: invalid inputs, quantity: {quantity}");
+                }
+
+                string customerId = _customerSerive.GetIdByToken(token);
+
+                await _customerSerive.UpdateCartUser(customerId, id, quantity);
+                return Ok("Updated cart items successfully");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+        }
+
+        // DELETE api/values/5
+        //Remove item in cart
+
+        [HttpDelete("delete-cart-item/{id}"), Authorize(Roles = "User")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last()!;
+                string customerId = _customerSerive.GetIdByToken(token);
+                await _customerSerive.RemoveCartItems(customerId, id);
+
+                return Ok("Deleted item successfully!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+        }
+
+        [HttpPost("create-order"), Authorize(Roles = "User")]
+        public async Task<IActionResult> CreateOder()
+        {
+            try
+            {
+                string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last()!;
+                string customerId = _customerSerive.GetIdByToken(token);
+
+                await _customerSerive.CreateOrder(customerId);
+                return Ok("Created order successfully!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+        }
+
+
+        [HttpGet("view-order-history"), Authorize(Roles = "User")]
+        public async Task<IActionResult> ViewOrderHistory()
+        {
+            try
+            {
+                string token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last()!;
+                string customerId = _customerSerive.GetIdByToken(token);
+
+                var orders = await _customerSerive.ViewOrdersHistory(customerId);
+
+                return Ok(orders);
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+        }
+    }
+}
---
+++ ASM1641-/Controllers/PublisherController.cs
@@ -0,0 +1,88 @@
+﻿using ASM1641_.IService;
+using ASM1641_.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASM1641_.Controllers
+{
+    [Authorize]
+    [Route("api/v1/publisher")]
+    public class PublisherController : Controller
+    {
+        private readonly IPublisherService _publisherService;
+        public PublisherController(IPublisherService publisherService)
+        {
+            this._publisherService = publisherService;
+        }
+
+
+        // GET: api/values
+        [HttpGet, Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Get()
+        {
+            var publishers = await _publisherService.GetPublishers();
+            return Ok(publishers);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}"), Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var publisher = await _publisherService.GetByID(id);
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(publisher);
+        }
+
+        // POST api/values
+        [HttpPost, Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Post([FromBody] Publisher aPublisher)
+        {
+            try
+            {
+                await _publisherService.CreatePublisher(aPublisher);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}"), Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Put(string id, [FromBody] Publisher aPublisher)
+        {
+            try
+            {
+                await _publisherService.UpdatePublisher(aPublisher, id);
+                return Ok("Updated successfully!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"An error occur: {e}");
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}"), Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                await _publisherService.RemovePublisher(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e}");
+            }
+
+        }
+    }
+}
